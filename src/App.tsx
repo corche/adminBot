22fb01064ca8b46ba6d96e96eaaa140609bdd7a1
corche <corche00@gmail.com>
2025-02-14 16:02:01 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
 	return (
 		<>
 			<Nav />
-			<div className="landing h-[80vh] pt-50 overflow-visible">
+			<div className="landing pt-50 overflow-visible">
 				<div className="flex flex-col items-center">
 					<div className="text-6xl font-bold text-center text-neutral-800">
 						디스코드에 필요한 기능을
@@ -22,6 +22,11 @@ export default function App() {
 						해결하세요!
 					</div>
 					<div className="w-[70vw] max-w-[1080px] h-[calc(70vw/2)] mt-10 bg-light-green-500 rounded-2xl"></div>
+				</div>
+			</div>
+			<div className="h-screen">
+				<div className="text-4xl font-bold text-center text-neutral-800">
+					올인원 디스코드봇
 				</div>
 			</div>
 		</>
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
+
 export default function Nav() {
+	const [scroll, setScroll] = useState(false);
+
+	const handleScroll = () => {
+		if (window.scrollY >= 50) {
+			setScroll(true);
+		} else {
+			setScroll(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
 		<>
-			<div className="w-screen bg-white/30 backdrop-blur-xl flex justify-between items-center h-[60px] fixed top-0 px-[10vw]">
-				<div className="text-2xl font-bold text-neutral-800">
+			<div
+				className={`${
+					scroll
+						? "w-[60vw] bg-white/30 backdrop-blur-xl mt-2"
+						: "w-[100vw] bg-white/0 px-[5vw]"
+				} fixed top-0 px-2 rounded-2xl h-[60px] flex justify-between items-center duration-500 ml-[50vw] translate-x-[-50%]`}
+			>
+				<div className="text-2xl ml-6 font-bold text-neutral-800">
 					관리봇
 				</div>
 				<div className="text-base flex w-[200px]">
 					<div
-						className={`cursor-pointer w-[100px] text-center text-light-green-100 relative after:absolute after:bottom-0 after:left-[50%] after:translate-x-[-50%] after:rounded-2xl after:w-[20px] after:h-[2px] after:bg-light-green-100`}
+						className={`cursor-pointer w-[100px] text-center text-light-green-100 relative after:absolute after:bottom-0 after:left-[50%] after:translate-x-[-50%] after:rounded-2xl after:w-[20px] after:h-[2px] after:bg-light-green-200`}
 					>
 						홈
 					</div>
@@ -15,7 +40,7 @@ export default function Nav() {
 						도움말
 					</div>
 				</div>
-				<button className="w-[100px] h-[42px] flex items-center justify-center text-base text-neutral-100 bg-light-green-100 rounded-xl">
+				<button className="cursor-pointer w-[100px] h-[42px] flex items-center justify-center text-base text-neutral-100 bg-light-green-200 hover:bg-light-green-100 rounded-xl duration-200">
 					초대하기
 				</button>
 			</div>
